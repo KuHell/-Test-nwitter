b39d10bb208e6f6e17eebc357f31bdc969eb97a2
KuHell <itkdgus489@gmail.com>
2022-12-08 16:05:52 +0900

    firebase연동작업

---
+++ firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDERID,
+  appId: process.env.APP_ID,
+};
+
+export default firebase.initializeApp(firebaseConfig);
