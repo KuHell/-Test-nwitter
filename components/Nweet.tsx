298ffb58c4360843a3bcd8ed144c8910c4136dc0
KuHell <itkdgus489@gmail.com>
2022-12-12 10:55:07 +0900

    CRUD기능

---
+++ components/Nweet.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { dbService } from "../firebase/firebase";
+
+const Nweet = ({ nweetObj }: any) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.nwet);
+
+  const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+
+  const onDelete = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      // delete 부분
+      await deleteDoc(NweetTextRef);
+    }
+  };
+
+  const toggleEdit = async () => {
+    setEditing((prev) => !prev);
+    //update 부분
+    // await updateDoc(NweetTextRef, {
+    //   text: newNweet,
+    // });
+  };
+
+  const onSubmit = async (e: any) => {
+    console.log("수정버튼");
+    e.preventDefault();
+    await updateDoc(NweetTextRef, {
+      nweet: newNweet,
+    });
+    setEditing(false);
+  };
+
+  const onChange = (e: any) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+
+  return (
+    <>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet || ""}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEdit}>취소</button>
+        </>
+      ) : (
+        <div>
+          <h4>{nweetObj.nweet}</h4>
+          <button onClick={onDelete}>삭제</button>
+          <button onClick={toggleEdit}>수정</button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Nweet;
--- pages/route/Home.tsx
+++ pages/route/Home.tsx
@@ -1,26 +1,31 @@
-import { addDoc, collection, getDocs, query } from "firebase/firestore";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { dbService } from "../../firebase/firebase";
-import { DocumentData } from "firebase/firestore";
-
-interface SnapshotData {
-  id: string;
-}
+import Nweet from "../../components/Nweet";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState();
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const dbNweets = await getDocs(collection(dbService, "nweets"));
-    dbNweets.forEach((document) => {
-      const nweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
-      setNweets((nweets) => [...nweets, nweetObject]);
+
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "nweets"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const nweetArr: any = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArr);
     });
-  };
+  }, []);
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
@@ -41,10 +46,6 @@ const Home = () => {
     setNweet(value);
   };
 
-  useEffect(() => {
-    getNweets();
-  }, []);
-
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -52,19 +53,14 @@ const Home = () => {
           type="text"
           placeholder="What's on your mind?"
           maxLength={120}
-          value={nweet}
+          value={nweet || ""}
           onChange={onChange}
         />
         <input type="submit" value="Nweet" />
       </form>
       <div>
         {nweets.map((nweet, key) => {
-          console.log("nweet: ", nweet);
-          return (
-            <div key={key}>
-              <h4>{nweet.nweet}</h4>
-            </div>
-          );
+          return <Nweet key={key} nweetObj={nweet} />;
         })}
       </div>
     </>
